7fa5a553c9641c4d76c96bbd046988e17711caa1
1_May <pelevinig525@gmail.com>
2023-09-08 03:54:53 +0300

    балуюсь/учусь_1

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def handler():
     return render_template('index.html')
-
 
 @app.route('/dynamic')
 def dynamic_content_handler():
@@ -22,11 +20,9 @@ def dynamic_content_handler():
     }
     return render_template('dynamic_index.html', content=context)
 
-
 @app.route('/greeting')
 def greeting_handler():
     return 'Hello new user'
-
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
