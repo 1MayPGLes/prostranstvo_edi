e3af7ad4d539c1aa51080bbd3d82e8b21f2f6987
1_May <pelevinig525@gmail.com>
2023-09-17 16:54:48 +0300

    Теперь три нажатии на лого со всех стрпниц переносит на лендинг, подключился к бд, добавил конфиг файл

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-import os
-from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+from config import host, port, user, database, password
 from flask import Flask, render_template, url_for, request, abort, flash, session, redirect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'lj8fw3nd88fasf854hskm454hnpdvu4e8'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://1324@localhost/hospital'
 
-db = SQLAlchemy(app)
+try:
+    conn = psycopg2.connect(
+        host=host,
+        port=port,
+        user=user,
+        database=database,
+        password=password
+    )
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM med_card;')
+    result = cur.fetchall()
+    print (result)
+except Exception as err:
+    print('[INFO] Error while working with PostgreSQL', err)
+finally:
+    if conn:
+        cur.close()
+        conn.close()
+        print('[INFO] PostgreSQL connection closed')
 
-@app.route('/', methods=['POST', 'GET'])
+
+@app.route('/')
 def index():
-    if request.method == 'POST' and request.form['username'] == "1_may" and request.form['password'] == "123":
-        session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
-
     professions = ['Анестезиолог', 'Гнойный хирург', 'Кардиолог', 'ЛОР', 'Невролог', 'Нейро-Кардио хирург', 'Онколог', 'Офтальмолог', 'Пульмонолог', 'Реаниматолог', 'Терапевт', 'Травматолог', 'Травматолог-ортопед', 'Хирург', 'Эндокринолог']
     services = [
         {'name': 'Диагностика', 'img': 'skoraya.png'},
@@ -44,12 +58,18 @@ def index():
     ]
     return render_template('index.html', title='Клиника «Измайловская» им. А.С. Багирова', services=services, professions=professions, doctors=doctors)
 
+@app.route('/registration', methods=['POST'])
+def registration():
+    if request.form['username'] == "1_may" and request.form['password'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return redirect(url_for('index'))
+
 @app.route('/profile/<username>', methods=['POST', 'GET'])
 def profile(username):
     if 'userLogged' not in session or session['userLogged'] != username:
         abort(401)
-
-    return render_template('profile.html', title='Личный кабинет {username}')
+    return render_template('profile.html', title='Личный кабинет', username=session['userLogged'])
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -58,7 +78,6 @@ def page_not_found(error):
 @app.errorhandler(401)
 def page_not_found(error):
     return render_template('unauthorized.html', title='Отказ в доступе')
-
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
