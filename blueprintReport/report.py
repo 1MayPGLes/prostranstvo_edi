0e27cec000f195ca8a87fe2ceee1b21706ea5346
1_May <pelevinig525@gmail.com>
2023-12-22 03:10:51 +0300

    доделал курсач

--- blueprintReport/report.py
+++ blueprintReport/report.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, request, render_template, current_app, session
+from flask import Blueprint, request, render_template, current_app, session, redirect, url_for
 from DB.funcDB import select, call_proc
 from DB.SQLProvider import SQLProvider
 from access import login_required, group_required
@@ -9,46 +9,46 @@ report = Blueprint('report', __name__, template_folder='templates')
 provider = SQLProvider(os.path.join(os.path.dirname(__file__), 'sql'))
 
 report_list = [
-    {'rep_name':'Выручка с фильмов', 'rep_id':'1'},
-    {'rep_name':'Выручка с залов', 'rep_id':'2'}
+    {'rep_name': 'Зарегистрированные в отделении пациенты', 'rep_id': '1'},
+    {'rep_name': 'Проведённые докторами отделения осмотры', 'rep_id': '2'}
 ]
 
-
 report_url = {
-    '1': {'create_rep':'bp_report.create_rep1', 'view_rep':'bp_report.view_rep1'},
-    '2': {'create_rep':'bp_report.create_rep2', 'view_rep':'bp_report.view_rep2'}
+    '1': {'create_rep': 'report.create_rep1', 'view_rep': 'report.view_rep1'},
+    '2': {'create_rep': 'report.create_rep2', 'view_rep': 'report.view_rep2'}
 }
 
-@report.route('/')
+@report.route('/', methods=['GET', 'POST'])
 @login_required
 @group_required
-def reports():
-    return render_template('report.html', title='Работа с отчётами')
-
-@report.route('/getReportPat', methods=['GET', 'POST'])
-@login_required
-@group_required
-def getReportPat():
+def start_report():
     if request.method == 'GET':
-        return render_template('getReport.html', title='Поиск отчётов о зарегистрированных в отделении пациентах')
+        return render_template('menu_report.html', report_list=report_list)
     else:
-        year = request.form.get('inputYear')
-        month = request.form.get('inputMonth')
-        _sql = provider.get('reportPat.sql', year=year, month=month)
-        getResult, getSchema = select(current_app.config['configDB'], _sql)
-        if len(getResult) == 0:
-            return render_template('getReport.html', title='Поиск отчётов о зарегистрированных в отделении пациентах', message='Отчёт не найден')
-        return render_template('getReport.html', title='Поиск отчётов о зарегистрированных в отделении пациентах', message='Вывод найденного отчёта', schema=getSchema, result=getResult)
+        rep_id = request.form.get('rep_id')
+        print('rep_id = ', rep_id)
+        if request.form.get('create_rep'):
+            url_rep = report_url[rep_id]['create_rep']
+        else:
+            url_rep = report_url[rep_id]['view_rep']
+        print('url_rep = ', url_rep)
+        return redirect(url_for(url_rep))
 
-@report.route('/createReportPat', methods=['GET', 'POST'])
+@report.route('/create_rep1', methods=['GET', 'POST'])
 @login_required
 @group_required
-def createReportPat():
+def create_rep1():
     if request.method == 'GET':
         return render_template('createReport.html', title='Создание отчётов о зарегистрированных в отделении пациентах')
     else:
         year = request.form.get('inputYear')
         month = request.form.get('inputMonth')
+        if (len(year) == 0) or (len(month) == 0):
+            message = 'Ошибка ввода. Заполните оба поля'
+            return render_template('createReport.html', title='Создание отчётов о зарегистрированных в отделении пациентах', message=message)
+        if (year.isdigit() != True) or (month.isdigit() != True):
+            message = 'Ошибка ввода'
+            return render_template('createReport.html', title='Создание отчётов о зарегистрированных в отделении пациентах', message=message)
         _sql = provider.get('reportPat.sql', year=year, month=month)
         createResult, createSchema = select(current_app.config['configDB'], _sql)
         if year and month and len(createResult) == 0:
@@ -62,30 +62,45 @@ def createReportPat():
                 return render_template('createReport.html', title='Создание отчётов о зарегистрированных в отделении пациентах', message='Не удалось создать отчёт')
             return render_template('createReport.html', title='Создание отчётов о зарегистрированных в отделении пациентах', message='Вывод созданного отчёта', schema=createSchema, result=createResult)
         return render_template('createReport.html', title='Создание отчётов о зарегистрированных в отделении пациентах', message='Такой отчёт уже существует')
-@report.route('/getReportIns', methods=['GET', 'POST'])
+
+@report.route('/view_rep1', methods=['GET', 'POST'])
 @login_required
 @group_required
-def getReportIns():
+def view_rep1():
     if request.method == 'GET':
-        return render_template('getReport.html', title='Поиск отчётов о проведённых докторами отделения осмотрах')
+        return render_template('getReport.html', title='Поиск отчётов о зарегистрированных в отделении пациентах')
     else:
         year = request.form.get('inputYear')
         month = request.form.get('inputMonth')
-        _sql = provider.get('reportIns.sql', year=year, month=month)
+        if (len(year) == 0) or (len(month) == 0):
+            message = 'Ошибка ввода. Заполните оба поля'
+            return render_template('getReport.html', title='Просмотр отчётов о зарегистрированных в отделении пациентах', message=message)
+        if (year.isdigit() != True) or (month.isdigit() != True):
+            message = 'Ошибка ввода'
+            return render_template('getReport.html', title='Просмотр отчётов о зарегистрированных в отделении пациентах', message=message)
+        _sql = provider.get('reportPat.sql', year=year, month=month)
         getResult, getSchema = select(current_app.config['configDB'], _sql)
         if len(getResult) == 0:
-            return render_template('getReport.html', title='Поиск отчётов о проведённых докторами отделения осмотрах', message='Отчёт не найден')
-        return render_template('getReport.html', title='Поиск отчётов о проведённых докторами отделения осмотрах', message='Вывод найденного отчёта', schema=getSchema, result=getResult)
+            message = 'Отчёт не найден'
+            return render_template('getReport.html', title='Просмотр отчётов о зарегистрированных в отделении пациентах', message=message)
+        message = 'Вывод найденного отчёта'
+        return render_template('getReport.html', title='Просмотр отчётов о зарегистрированных в отделении пациентах', message=message, schema=getSchema, result=getResult)
 
-@report.route('/createReportIns', methods=['GET', 'POST'])
+@report.route('/create_rep2', methods=['GET', 'POST'])
 @login_required
 @group_required
-def createReportIns():
+def create_rep2():
     if request.method == 'GET':
         return render_template('createReport.html', title='Создание отчётов о проведённых докторами отделения осмотрах')
     else:
         year = request.form.get('inputYear')
         month = request.form.get('inputMonth')
+        if (len(year) == 0) or (len(month) == 0):
+            message = 'Ошибка ввода. Заполните оба поля'
+            return render_template('createReport.html', title='Создание отчётов о проведённых докторами отделения осмотрах', message=message)
+        if (year.isdigit() != True) or (month.isdigit() != True):
+            message = 'Ошибка ввода'
+            return render_template('createReport.html', title='Создание отчётов о проведённых докторами отделения осмотрах', message=message)
         _sql = provider.get('reportIns.sql', year=year, month=month)
         createResult, createSchema = select(current_app.config['configDB'], _sql)
         if year and month and len(createResult) == 0:
@@ -99,3 +114,26 @@ def createReportIns():
                 return render_template('createReport.html', title='Создание отчётов о проведённых докторами отделения осмотрах', message='Не удалось создать отчёт')
             return render_template('createReport.html', title='Создание отчётов о проведённых докторами отделения осмотрах', message='Вывод созданного отчёта', schema=createSchema, result=createResult)
         return render_template('createReport.html', title='Создание отчётов о проведённых докторами отделения осмотрах', message='Такой отчёт уже существует')
+
+@report.route('/view_rep2', methods=['GET', 'POST'])
+@login_required
+@group_required
+def view_rep2():
+    if request.method == 'GET':
+        return render_template('getReport.html', title='Просмотр отчётов о проведённых докторами отделения осмотрах')
+    else:
+        year = request.form.get('inputYear')
+        month = request.form.get('inputMonth')
+        if (len(year) == 0) or (len(month) == 0):
+            message = 'Ошибка ввода. Заполните оба поля'
+            return render_template('getReport.html', title='Просмотр отчётов о проведённых докторами отделения осмотрах', message=message)
+        if (year.isdigit() != True) or (month.isdigit() != True):
+            message = 'Ошибка ввода'
+            return render_template('getReport.html', title='Просмотр отчётов о проведённых докторами отделения осмотрах', message=message)
+        _sql = provider.get('reportIns.sql', year=year, month=month)
+        getResult, getSchema = select(current_app.config['configDB'], _sql)
+        if len(getResult) == 0:
+            message = 'Отчёт не найден'
+            return render_template('getReport.html', title='Просмотр отчётов о проведённых докторами отделения осмотрах', message=message)
+        message = 'Вывод найденного отчёта'
+        return render_template('getReport.html', title='Просмотр отчётов о проведённых докторами отделения осмотрах', message=message, schema=getSchema, result=getResult)
--- test.py
+++ test.py
@@ -1,59 +1,107 @@
-from flask import json
-from DB.funcDB import select, insert
+@report.route('/create_rep1', methods=['GET', 'POST'])
+@login_required
+@group_required
+def create_rep1():
+    if request.method == 'GET':
+        print("GET_create")
+        return render_template('report_create.html', name_rep="выручке с фильма", title='Отчет 1')
+    else:
+        print(current_app.config['db_config'])
+        print("POST_create")
+        try:
+            rep_month = int(request.form.get('input_month'))
+            rep_year = int(request.form.get('input_year'))
+        except ValueError:
+            return render_template('error.html', error="Параметры отчёта введены неверно! "
+                                                           "Введите год в формате XXXX, месяц в формате XX")
+        print("Loading...")
+        if rep_year and rep_month and 2000 < rep_year <= 2023 and 0 < rep_month <= 12:
+            _sql = provider.get('check_rep1.sql', in_year=rep_year, in_month=rep_month)
+            product_result, schema = select(current_app.config['db_config'], _sql)
+            print(product_result)
+            if (product_result[0][0] > 0):
+                return render_template('error.html', error="Такой отчёт уже существует")
+            else:
+                res = call_proc(current_app.config['db_config'], 'stonks_report', rep_year, rep_month)
+                print('res=', res)
+                return render_template('report_created.html', rep_month=rep_month, rep_year=rep_year, name_rep="выручке с фильма")
+        else:
+            return render_template('error.html', error="Вы допустили ошибку! "
+                                                           "Введите год в формате XXXX, месяц в формате XX")
 
-with open('data/configDB.json', 'r') as f:
-    configDB = json.load(f)
 
-# product = f"""
-#             SELECT prod_name
-#             FROM product
-#         """
-#
-# productResult, productSchema = select(configDB, product)
-#
-# productResult = [item[0] for item in productResult]
-#
-# print(productResult)
-# print(productSchema)
+@report.route('/view_rep1', methods=['GET', 'POST'])
+@login_required
+@group_required
+def view_rep1():
+    if request.method == 'GET':
+        return render_template('view_rep.html', name_rep='выручке с фильма')
+    else:
+        try:
+            rep_month = int(request.form.get('input_month'))
+            rep_year = int(request.form.get('input_year'))
+        except ValueError:
+            return render_template('error.html', error="Параметры запроса введены неверно! "
+                                                           "Введите год в формате XXXX, месяц в формате XX")
+        if rep_year and rep_month and 2000 < rep_year <= 2023 and 0 < rep_month <= 12:
+            _sql = provider.get('rep1.sql', in_year=rep_year, in_month=rep_month)
+            product_result, schema = select(current_app.config['db_config'], _sql)
+            if product_result:
+                return render_template('result_rep1.html', schema=schema, result=product_result, rep_month=rep_month, rep_year=rep_year)
+            else:
+                return render_template('error.html', error="Такой отчёт не был создан")
+        else:
+            return render_template('error.html', error="Вы допустили ошибку! "
+                                                           "Введите год в формате XXXX, месяц в формате XX")
 
+@report.route('/create_rep2', methods=['GET', 'POST'])
+@login_required
+@group_required
+def create_rep2():
+    if request.method == 'GET':
+        print("GET_create")
+        return render_template('report_create.html', name_rep="выручке с зала", title='Отчет 2')
+    else:
+        try:
+            rep_month = int(request.form.get('input_month'))
+            rep_year = int(request.form.get('input_year'))
+        except ValueError:
+            return render_template('error.html', error="Параметры запроса введены неверно! "
+                                                           "Введите год в формате XXXX, месяц в формате XX")
+        if rep_year and rep_month and 2000 < rep_year <= 2023 and 0 < rep_month <= 12:
+            _sql = provider.get('check_rep2.sql', in_year=rep_year, in_month=rep_month)
+            product_result, schema = select(current_app.config['db_config'], _sql)
+            print(product_result)
+            if (product_result[0][0] > 0):
+                return render_template('error.html', error="Такой отчёт уже существует")
+            else:
+                res = call_proc(current_app.config['db_config'], 'places_report', rep_year, rep_month)
+                print('res=', res)
+                return render_template('report_created.html', rep_month=rep_month, rep_year=rep_year, name_rep="выручке с зала")
+        else:
+            return render_template('error.html', error="Вы допустили ошибку! "
+                                                           "Введите год в формате XXXX, месяц в формате XX")
 
-specialist = f"""
-                SELECT *
-                FROM doctor
-                WHERE id_doc=123124312
-            """
-specialistResult, specialistSchema = select(configDB, specialist)
-
-specialistResult = [item[0] for item in specialistResult]
-
-print(specialistResult)
-print(specialistSchema)
-print(len(specialistResult))
-
-x = 5
-y = "Hello, world!"
-z = 3.14
-a = None
-
-print(isinstance(x, int))    # True
-print(isinstance(y, str))    # True
-print(isinstance(z, float))  # True
-print(a)  # True
-
-def choose_noun_form(age):
-    last_two_digits = age % 100
-
-    if 11 <= last_two_digits <= 19:
-        return "лет"
-    last_digit = last_two_digits % 10
-    if last_digit == 1:
-        return "год"
-    if 2 <= last_digit <= 4:
-        return "года"
-    return "лет"
-
-print(choose_noun_form(1))
-print(choose_noun_form(14))
-print(choose_noun_form(21))
-print(choose_noun_form(24))
-print(choose_noun_form(27))
+@report.route('/view_rep2', methods=['GET', 'POST'])
+@login_required
+@group_required
+def view_rep2():
+    if request.method == 'GET':
+        return render_template('view_rep.html', name_rep="выручке с зала")
+    else:
+        try:
+            rep_month = int(request.form.get('input_month'))
+            rep_year = int(request.form.get('input_year'))
+        except ValueError:
+            return render_template('error.html', error="Параметры запроса введены неверно! "
+                                                           "Введите год в формате XXXX, месяц в формате XX")
+        if rep_year and rep_month and 2000 < rep_year <= 2023 and 0 < rep_month <= 12:
+            _sql = provider.get('rep2.sql', in_year=rep_year, in_month=rep_month)
+            product_result, schema = select(current_app.config['db_config'], _sql)
+            if product_result:
+                return render_template('result_rep1.html', schema=schema, result=product_result, rep_month=rep_month, rep_year=rep_year)
+            else:
+                return render_template('error.html', error="Такой отчёт не был создан")
+        else:
+            return render_template('error.html', error="Вы допустили ошибку! "
+                                                           "Введите год в формате XXXX, месяц в формате XX")
